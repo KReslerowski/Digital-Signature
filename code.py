ad3c6e0b399d0ad229e30e60ceddbe15a6b3a32f
KReslerowski <96911254+KReslerowski@users.noreply.github.com>
2022-06-13 21:08:34 +0200

    Update code.py

--- code.py
+++ code.py
@@ -2,16 +2,10 @@ import base64
 from Crypto import Random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Hash import SHA256
+from Crypto.Signature import pss
 import hashlib
-
-text_file = open("tekst1.txt", "rb")
-
-text_file = text_file.read()
-
-output = hashlib.sha3_256()
-output.update(text_file)
-
-print(output.digest())
+import os, sys
 
 def toBase64(string):
     return base64.b64encode(string)
@@ -23,36 +17,47 @@ def generate_keys():
 
 pri, pub = generate_keys()
 
-text_file_2 = open("tekst2.txt", "rb")
-text_file_2 = text_file_2.read()
+path = "D:\Studia\Sem_6\Bezpieczenstwo_sieci\encryption"
+dirs = os.listdir(path)
 
-output_2 = hashlib.sha3_256()
-output_2.update(text_file_2)
-
-print(output_2.digest())
-
-cipher = PKCS1_OAEP.new(pub)
-M = cipher.encrypt(text_file)
-print(M, "\n")
-
-cipher = PKCS1_OAEP.new(pri)
-m = cipher.decrypt(M)
-print(m, "\n")
+for file in dirs:
+    print(file)
     
-""" Convert hash to hexigest string """
-    
-hex_dig = output.hexdigest()
-print(hex_dig)
-hex_dig2 = output_2.hexdigest()
-print(hex_dig2)
+download_file = input("Jaki plik chcesz zaszyfrować? ")
 
-""" VALIDATION """
-if hex_dig == hex_dig2:
-    print("It's ok")
-    cipher = PKCS1_OAEP.new(pri)
-    m = cipher.decrypt(M)
-    
-    print(m, "\n")
-else:
-    print("error!")
+open_file = open(download_file, "rb")
 
+read_file = open_file.read()
+#print(read_file)
+
+hash = SHA256.new()
+hash.update(read_file)
+signature = pss.new(pri).sign(hash)
+
+fsave=open("signedMessage.txt", "wb")
+fsave.write(signature)
+fsave.close()
+
+path = "D:\Studia\Sem_6\Bezpieczenstwo_sieci\encryption"
+dirs = os.listdir(path)
+
+for file in dirs:
+    print(file)
+
+download_file_verify = input("Jaki plik chcesz sprawdzic? ")
+download_file_sign = input("Wybierz podpisany plik ")
+
+file_verify = open(download_file_verify, "rb")
+file_verify_open = file_verify.read()
+
+hash2 = SHA256.new()
+hash2.update(file_verify_open)
+verifier = pss.new(pub)
+file_sign = open(download_file_sign, "rb")
+file_sign_open = file_sign.read()
+
+try:
+    verifier.verify(hash2, file_sign_open)
+    print("Podpis sie zgadza!")
+except(ValueError, TypeError):
+    print("Podpis sie nie zgadza, zly plik!")
